f4a6d0849e976bbdde23203f2ced992b0b80959d
Saroj <sarojregmi.official@gmail.com>
2024-05-06 05:17:01 +0000

    Added some metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,6 +5,19 @@ import "./globals.css";
 export const metadata: Metadata = {
     title: "AppliCat",
     description: "Write applications the way they are ment to be written.",
+    icons: [], // icons are yet to come
+    keywords: [
+        "Us application",
+        "consulting",
+        "application",
+        "guidance",
+        "Higher acceptance",
+        "scholarship"
+    ],
+    openGraph: {
+        title: "AppliCat",
+        description: "Write applications the way they are ment to be written.",
+    }
 };
 
 export default function RootLayout({
