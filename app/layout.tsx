84ff994c881e07fcda9f526e31381146a8f5127b
Aashish Panthi <aashishpanthi11@gmail.com>
2024-05-09 22:06:16 +0545

    add dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,32 @@
+import Providers from "@/components/layout/providers";
+import { Toaster } from "@/components/ui/toaster";
+import "@uploadthing/react/styles.css";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { getServerSession } from "next-auth";
 
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "AppliCat",
-    description: "Write applications the way they are ment to be written.",
-    icons: [], // icons are yet to come
-    keywords: [
-        "Us application",
-        "consulting",
-        "application",
-        "guidance",
-        "Higher acceptance",
-        "scholarship"
-    ],
-    openGraph: {
-        title: "AppliCat",
-        description: "Write applications the way they are ment to be written.",
-    }
+  title: "Next Shadcn",
+  description: "Basic dashboard with Next.js and Shadcn",
 };
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    );
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerSession();
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} overflow-hidden`}>
+        <Providers session={session}>
+          <Toaster />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-    return (
-        <div className="text-8xl font-medium text-white flex h-screen w-screen justify-center items-center bg-gradient-to-bl from-orange-700 to-pink-900">
-            AppliCat
-        </div>
-    );
-}
