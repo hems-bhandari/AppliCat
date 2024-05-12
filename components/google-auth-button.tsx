b2b6996086e2e236360acf28372b536cefd830fd
Saroj <sarojregmi.official@gmail.com>
2024-05-12 16:35:27 +0000

    Added google auth

---
+++ components/google-auth-button.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { Button } from "./ui/button";
+import { Icons } from "./icons";
+
+export default function GoogleSignInButton() {
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get("callbackUrl");
+
+    return (
+        <Button
+            className="w-full"
+            variant="outline"
+            type="button"
+            onClick={() =>
+                signIn("google",
+                    {
+                        callbackUrl: callbackUrl ?? "/dashboard",
+                    }
+                )
+            }
+        >
+            <Icons.google className="mr-2 h-4 w-4" />
+            Continue with Google
+        </Button>
+    );
+}
