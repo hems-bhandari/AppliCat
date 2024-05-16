701f0e95d11eef6c276438c0f5bd3452c33501e5
Hemanta Bhandari <hemsbhandari04@gmail.com>
2024-05-16 21:25:41 +0545

    Update layout.tsx

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -3,8 +3,8 @@ import Sidebar from "@/components/layout/sidebar";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Next Shadcn Dashboard Starter",
-  description: "Basic dashboard with Next.js and Shadcn",
+  title: "AppliCat | Dashboard",
+  description: "Get purrsonalized guidance with AppliCat",
 };
 
 export default function DashboardLayout({
