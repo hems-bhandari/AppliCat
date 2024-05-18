9bd7ed66e5a96ec8fce7ac0a9251c9be1243a035
Saroj <sarojregmi.official@gmail.com>
2024-05-18 16:48:51 +0000

    Added Katibeh font

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import LandingNav from "@/components/landing/LandingNav";
+{/* import { katibeh } from "@/lib/fonts"; */ }
 import React from "react";
 
+
 const Landing = () => {
-  return (
-    <div className="bg-black min-h-screen w-full">
-      <LandingNav />
-    </div>
-  );
+    return (
+        <div className="bg-black min-h-screen w-full">
+            <LandingNav />
+
+
+        </div>
+    );
 };
 
 export default Landing;
--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,11 +1,16 @@
-import { Inter, Roboto } from "next/font/google";
+import { Inter, Roboto, Katibeh } from "next/font/google";
 
 export const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400"],
+    subsets: ["latin"],
+    weight: ["400"],
 });
 
 export const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500"],
+    subsets: ["latin"],
+    weight: ["400", "500"],
 });
+
+export const katibeh = Katibeh({
+    subsets: ["latin"],
+    weight: ["400"],
+})
