4b6cf2db0b17f3726e2f682c6c7729303258ed4d
Saroj <sarojregmi.official@gmail.com>
2024-05-18 16:09:35 +0000

    Added User Modal

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,19 @@
 import { authOptions } from "@/lib/auth-options";
 import NextAuth from "next-auth/next";
+import { NextRequest, NextResponse } from "next/server";
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export function GET(req: NextRequest, res: NextResponse) {
+    try {
+        console.log(req)
+        handler();
+    } catch (e) {
+
+    }
+}
+
+export {
+    handler as POST
+};
+
