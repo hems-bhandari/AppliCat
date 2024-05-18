8e0a9eb9efff13dcc86e7fc35c0d811aef17e209
Saroj <sarojregmi.official@gmail.com>
2024-05-18 16:27:25 +0000

    Updated the handler as default

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,16 +4,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 const handler = NextAuth(authOptions);
 
-export function GET(req: NextRequest, res: NextResponse) {
-    try {
-        console.log(req)
-        handler();
-    } catch (e) {
-
-    }
-}
+// export function GET(req: NextRequest, res: NextResponse) {
+//     try {
+//         console.log(req)
+//         return handler;
+//     } catch (e) {
+//
+//     }
+// }
 
 export {
+    handler as GET,
     handler as POST
 };
 
