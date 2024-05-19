5c9084ea278560cc99372f7daba7a894346d4cc6
Saroj <sarojregmi.official@gmail.com>
2024-05-19 02:44:17 +0000

    Middle ware to protect public and private routes complete

--- middleware.ts
+++ middleware.ts
@@ -2,5 +2,31 @@
 // https://next-auth.js.org/configuration/nextjs#middleware
 // https://nextjs.org/docs/app/building-your-application/routing/middleware
 
+import { NextRequest, NextResponse } from "next/server";
+
 export { default } from "next-auth/middleware";
-export const config = { matcher: ["/dashboard/:path*"] };
+
+export function middleware(req: NextRequest) {
+    const pathname = req.nextUrl.pathname;
+
+    const publicPaths = ["/", "/auth"];
+    const isPublic = publicPaths.includes(pathname);
+
+    const token =
+        (req.cookies.get("next-auth.session-token"));
+
+    if (!isPublic && !token)
+        return NextResponse.redirect(new URL('/auth', req.nextUrl));
+
+    if (pathname === "/auth" && token)
+        return NextResponse.redirect(new URL('/dashboard', req.nextUrl))
+}
+
+export const config = {
+    matcher: [
+        "/dashboard/:path*",
+        "/dashboard",
+        "/",
+        "/auth"
+    ]
+};
