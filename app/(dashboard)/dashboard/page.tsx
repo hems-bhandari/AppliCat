1d7ae1232ae05d65a9e17a9f0d35d0752de2391b
Saroj <sarojregmi.official@gmail.com>
2024-05-20 16:03:08 +0000

    Cleaned the code

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,4 @@
-import { CalendarDateRangePicker } from "@/components/date-range-picker";
-import { Overview } from "@/components/overview";
 import { SessionHistory } from "@/components/SessionHistory";
-import { Button } from "@/components/ui/button";
 import {
     Card,
     CardContent,
@@ -10,7 +7,7 @@ import {
     CardTitle,
 } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
 
 export default function page() {
     return (
@@ -20,20 +17,8 @@ export default function page() {
                     <h2 className="text-3xl font-bold tracking-tight">
                         Hi, Applicant! Welcome back 👋
                     </h2>
-                    {/* <div className="hidden md:flex items-center space-x-2">
-            <CalendarDateRangePicker />
-            <Button>Download</Button>
-          </div> */}
                 </div>
                 <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-7">
-                    {/* <Card className="col-span-4">
-                <CardHeader>
-                  <CardTitle>Overview</CardTitle>
-                </CardHeader>
-                <CardContent className="pl-2">
-                  <Overview />
-                </CardContent>
-              </Card> */}
 
                     <Card className="col-span-4 md:col-span-3">
                         <CardHeader>
@@ -46,41 +31,9 @@ export default function page() {
                     </Card>
                 </div>
                 <Tabs defaultValue="overview" className="space-y-4">
-                    {/* <TabsList>
-            <TabsTrigger value="overview">Overview</TabsTrigger>
-            <TabsTrigger value="analytics" disabled>
-              Analytics
-            </TabsTrigge10>
-          </TabsList> */}
                     <TabsContent value="overview" className="space-y-4 mt-10">
                         <h4 className="font-bold ">Suggested Sessions for You : </h4>
                         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                            {/* <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Total Revenue
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">$45,231.89</div>
-                  <p className="text-xs text-muted-foreground">
-                    +20.1% from last month
-                  </p>
-                </CardContent>
-              </Card> */}
-
                             <Card>
                                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                                     <CardTitle className="text-sm font-medium">
@@ -124,6 +77,9 @@ export default function page() {
                         </div>
                     </TabsContent>
                 </Tabs>
+            </div>
+            <div className="aashish">
+
             </div>
         </ScrollArea>
     );
