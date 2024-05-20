f9b2b3e9fd1a358256f7035f07702f43a0164805
Saroj <sarojregmi.official@gmail.com>
2024-05-20 17:02:41 +0000

    Admin

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -15,7 +15,7 @@ export default function page() {
             <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
                 <div className="flex items-center justify-between space-y-2">
                     <h2 className="text-3xl font-bold tracking-tight">
-                        Hi, Applicant! Welcome back 👋
+                        Hi, Admin! Welcome back 👋
                     </h2>
                 </div>
                 <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-7">
