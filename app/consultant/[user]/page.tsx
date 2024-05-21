64a002c7f40d0dc857ad75f4bbc2cae958ad3168
Saroj <sarojregmi.official@gmail.com>
2024-05-21 17:30:39 +0000

    Dynamic route for consultant added

---
+++ app/consultant/[user]/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from 'react'
+
+const ConsultantPage = (
+    { params: { user } }:
+        { params: { user: string } }
+) => {
+
+    return (
+        <div>
+            You can book me here {user}
+        </div>
+    )
+}
+
+export default ConsultantPage;
