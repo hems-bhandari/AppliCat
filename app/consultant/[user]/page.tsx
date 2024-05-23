529808c7b8839139d320c043d22b2bb1bb5ee3d6
Hemanta Bhandari <hemsbhandari04@gmail.com>
2024-05-23 21:34:52 +0545

    Add booking page + Browse feature

--- app/consultant/[user]/page.tsx
+++ app/consultant/[user]/page.tsx
@@ -1,16 +1,65 @@
 "use client";
 
-import React from 'react'
+import React from "react";
+import { CalendarIcon, Divide } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import { Card } from "@/components/ui/card";
+import { add, format } from "date-fns";
 
-const ConsultantPage = (
-    { params: { user } }:
-        { params: { user: string } }
-) => {
-    return (
-        <div>
-            You can book me here {user}
-        </div>
-    )
+interface DateType {
+  justDate: Date | null;
+  dateTime: Date | null;
 }
+
+const ConsultantPage = ({ params: { user } }: { params: { user: string } }) => {
+  const [date, setDate] = React.useState<DateType>({
+    justDate: null,
+    dateTime: null,
+  });
+  const getTimes = () => {
+    if (!date.justDate) return;
+    const { justDate } = date;
+
+    const beginning = add(justDate, { hours: 19 });
+    const end = add(justDate, { hours: 21 });
+    const interval = 15;
+
+    const times = [];
+    for (let i = beginning; i <= end; i = add(i, { minutes: interval })) {
+      times.push(i);
+    }
+    return times;
+  };
+  const times = getTimes();
+  return (
+    
+    <div className="flex flex-col items-center justify-center h-screen">
+      {/* <CalendarIcon /> */}
+      <h2> Book a session with {user}</h2>
+      <div className="flex">
+      <Calendar
+        fromDate={new Date()}
+        onDayClick={(date) => setDate((prev) => ({ ...prev, justDate: date }))}
+        mode="single"
+      />
+      {date.justDate && (
+        <div className="flex flex-col gap-4">
+          {times?.map((time, index) => (
+            <div key={`time-${index}`} className="rounded-sm bg-grey-100 p-2">
+              <button
+
+                type="button"
+                onClick={() => setDate((prev) => ({ ...prev, dateTime: time }))}
+              >
+                {format(time, "kk:mm")}
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+    </div>
+  );
+};
 
 export default ConsultantPage;
