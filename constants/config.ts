8ec4fb45ef3d1c473f77f2375a9f8df3eb48d5dd
Hemanta Bhandari <hemsbhandari04@gmail.com>
2024-05-24 13:56:40 +0545

    few changes

---
+++ constants/config.ts
@@ -0,0 +1,4 @@
+export const CONSULTANT_AVAILABILITY_BEGINNING_HOUR = 19; // in hrs 24-hr time
+export const CONSULTANT_AVAILABILITY_ENDING_HOUR = 21;
+export const CONSULTANT_AVAILABILITY_INTERVAL = 15; // in minutes
+
