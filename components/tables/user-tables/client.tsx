55d79aba5293960b41abda64ff8cd093c1db14aa
Hemanta Bhandari <hemsbhandari04@gmail.com>
2024-05-24 23:18:31 +0545

    Finalize Sessions Tab

--- components/tables/user-tables/client.tsx
+++ components/tables/user-tables/client.tsx
@@ -19,15 +19,15 @@ export const UserClient: React.FC<ProductsClientProps> = ({ data }) => {
     <>
       <div className="flex items-start justify-between">
         <Heading
-          title={`Users (${data.length})`}
-          description="Manage users (Client side table functionalities.)"
+          title={`Session History`}
+          description={`Here's a list of all the sessions you have given. ${data.length} sessions so far!`}
         />
-        <Button
+        {/* <Button
           className="text-xs md:text-sm"
           onClick={() => router.push(`/dashboard/user/new`)}
         >
           <Plus className="mr-2 h-4 w-4" /> Add New
-        </Button>
+        </Button> */}
       </div>
       <Separator />
       <DataTable searchKey="name" columns={columns} data={data} />
