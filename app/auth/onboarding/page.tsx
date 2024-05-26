cbb488da69d4cc34658501740ed651b51e71fc70
Shreejay Subedi <shreejaysubedi@gmail.com>
2024-05-26 10:36:33 +0545

    Polished Onboarding Page

--- app/auth/onboarding/page.tsx
+++ app/auth/onboarding/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { FormEvent, useEffect, useRef, useState } from 'react';
 import Image from "next/image";
+import Typewriter from 'typewriter-effect';
 
 // fonts
-import { roboto } from "@/lib/fonts";
+import { katibeh, roboto } from "@/lib/fonts";
 import { signIn, useSession } from 'next-auth/react';
 
 const MyComponent = () => {
@@ -73,25 +74,52 @@ const MyComponent = () => {
                     objectPosition="center"
                     className="z-0"
                 />
+                <div className="absolute top-0 left-0 z- m-8">
+                     <Image
+                        src="/onboard_logo.svg" 
+                        alt="top left image"
+                        width={90} 
+                        height={90} 
+                    /> 
+                </div>
+                <div className="absolute inset-0 flex items-center m-8 -mt-12">
+                <Image
+                        src="/text.svg" 
+                        alt="top left image"
+                        width={450} 
+                        height={450} 
+                    /> 
+                </div>
+                <div className="absolute inset-0 flex items-center m-10" style={{ marginRight: "50px", height: '120px', marginTop:"480px" }}>
+                    <div className="typewriter-container" style={{ height: '100%', overflow: 'hidden' }}>
+                        <Typewriter
+                            options={{
+                                strings: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </div>
+                </div>
             </div>
             <div className="absolute right-0 top-0 w-full md:w-1/2 h-full flex items-center justify-center">
-                <div className="max-w-lg mx-auto md:w-4/5 px-4 py-8 bg-white rounded-lg shadow-lg" style={{ backgroundColor: '#101418' }}>
-                    <form onSubmit={handleSubmit} className='*:mt-4 *:space-y-1'>
+                <div className="max-w-lg mx-auto md:w-2/3 px-4 py-6 bg-white rounded-lg shadow-lg" style={{ backgroundColor: '#101418' }}>
+                    <form onSubmit={handleSubmit} className='*:mt-4 *:space-y-2/3'>
                         <div className="input">
-                            <label htmlFor="userName">Full Name</label>
+                            <label htmlFor="userName" className="text-sm md:text-xs">Full Name</label>
                             <input
                                 type="text"
                                 id="userName"
                                 name="userName"
                                 defaultValue={autofill && autofill.userName || ""}
                                 placeholder="User Name"
-                                className={`w-full px-3 py-2 rounded-md focus:outline-none `}
+                                className={`w-full px-3 py-1 rounded-md focus:outline-none placeholder:text-sm`}
                                 required
                             />
                         </div>
 
                         <div className="input">
-                            <label htmlFor="email">
+                            <label htmlFor="email" className="text-sm md:text-xs">
                                 Email
                             </label>
                             <input
@@ -102,42 +130,42 @@ const MyComponent = () => {
                                 defaultValue={autofill && autofill?.email || ""}
                                 disabled
                                 maxLength={10}
-                                className={`w-full px-3 py-2 rounded-md disabled:placeholder:text-gray-400 text-gray-400 disabled:cursor-not-allowed `}
+                                className={`w-full px-3 py-1 rounded-md disabled:placeholder:text-sm text-gray-400 disabled:cursor-not-allowed `}
                                 required
                             />
                         </div>
 
                         <div className="input">
-                            <label htmlFor="phonenumber"> Phone Number</label>
+                            <label htmlFor="phonenumber" className="text-sm md:text-xs"> Phone Number</label>
                             <input
                                 type="tel"
                                 id="phonenumber"
                                 name="phonenumber"
                                 placeholder="Phone Number"
                                 maxLength={10}
-                                className={`w-full px-3 py-2 rounded-md focus:outline-none `}
+                                className={`w-full px-3 py-1 rounded-md focus:outline-none placeholder:text-sm `}
                                 required
                             />
                         </div>
 
 
                         <div className="input">
-                            <label htmlFor="highschool">High School Name</label>
+                            <label htmlFor="highschool" className="text-sm md:text-xs">High School Name</label>
                             <input
                                 type="text"
                                 id="highschool"
                                 name="highschool"
                                 placeholder="Highschool/College"
-                                className={`w-full px-3 py-2 rounded-md focus:outline-none `}
+                                className={`w-full px-3 py-1 rounded-md focus:outline-none placeholder:text-sm `}
                                 required
                             />
                         </div>
 
                         <div className="input">
-                            <label htmlFor="education">Education</label>
+                            <label htmlFor="education" className="text-sm md:text-xs">Education</label>
                             <select id="education"
                                 name="education"
-                                className={`w-full px-3 py-2 rounded-md focus:outline-none  invalid:border-red-500'}`}
+                                className={`w-full px-3 py-1 rounded-md focus:outline-none placeholder:text-sm invalid:border-red-500'}`}
                                 required
                             >
                                 <option value="">Select Curriculum</option>
@@ -149,27 +177,27 @@ const MyComponent = () => {
                         </div>
 
                         <div className="input">
-                            <label htmlFor="gpa">GPA/Grades</label>
+                            <label htmlFor="gpa" className="text-sm md:text-xs">GPA/Grades</label>
                             <input
                                 type="gpa"
                                 maxLength={4}
                                 id="gpa"
                                 name="gpa"
                                 placeholder="Highschool GPA/Grades"
-                                className={`w-full px-3 py-2 rounded-md focus:outline-none`}
+                                className={`w-full px-3 py-1 rounded-md focus:outline-none placeholder:text-sm`}
                                 required
                             />
                         </div>
 
                         <div className="input">
-                            <label htmlFor="sat"> SAT Score</label>
+                            <label htmlFor="sat" className="text-sm md:text-xs"> SAT Score</label>
                             <input
                                 type="sat"
                                 maxLength={4}
                                 id="sat"
                                 name="sat"
                                 placeholder="SAT Score (if taken) "
-                                className={`w-full px-3 py-2 rounded-md focus:outline-none `}
+                                className={`w-full px-3 py-1 rounded-md focus:outline-none placeholder:text-sm`}
                                 required
                             />
                         </div>
@@ -180,9 +208,9 @@ const MyComponent = () => {
                         <button
                             type="submit"
                             className={`w-full bg-gradient-to-r from-[#F4442A] to-[#F1314A]  rounded-md flex items-center justify-center py-2 px-4 text-white  ${roboto.className} `}
-                            style={{ marginTop: '40px' }}
+                            style={{ marginTop: '25px' }}
                         >
-                            <span className='font-bold'>SUBMIT</span>
+                            <span className='font-bold'>REGISTER</span>
                         </button>
                     </form>
                 </div>
