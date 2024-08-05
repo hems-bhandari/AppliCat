953359feec9bc615e93975123a2911a01ab9b6f9
sarojregmi200 <sarojregmi.official@gmail.com>
2024-08-05 10:07:51 +0545

    Updated the consultant page.

--- app/(dashboard)/consultant/page.tsx
+++ app/(dashboard)/consultant/page.tsx
@@ -1,34 +1,21 @@
+"use client";
+
 import { ScrollArea } from "@/components/ui/scroll-area";
 import SideInfoBar from "@/components/SideInfoBar";
 import DashboardCurrentTime from "@/components/DashboardCurrentTime";
+import { PastSessions, UpcommingSession } from "@/components/sessions/";
 
-import { authOptions } from "@/lib/auth-options";
-import { getServerSession } from "next-auth";
-import { PastSessions, UpcomingSessions } from "@/components/sessions/PreviousSession";
-
-
-export default async function page() {
-    const userSession = await getServerSession(authOptions);
-
-    if (!userSession?.user)
-        return;
-
-    const userData = {
-        userType: userSession.user.userType as "Applicant" | "Consultant",
-        userId: userSession.user._id,
-    }
-
+export default function ApplicantPage() {
     return (
         <ScrollArea className="h-full">
             <div className="flex w-full py-8 flex-wrap flex-col-reverse md:flex-row">
                 <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
                     <DashboardCurrentTime />
 
-                    <PastSessions {...userData} />
-                    <UpcomingSessions {...userData} />
+                    <UpcommingSession />
+                    <PastSessions />
                 </div>
-
-                {/* <SideInfoBar userSession={userSession} /> */}
+                <SideInfoBar />
             </div>
         </ScrollArea>
     );
