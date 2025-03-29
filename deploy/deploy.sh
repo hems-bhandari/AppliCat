9afafb2e6b468db31ad9d131e4734bfd9c957309
sarojregmi200 <sarojregmi.official@gmail.com>
2025-03-29 22:24:34 +0545

    feat: Added a new way of deploying the project

---
+++ deploy/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Deploys the current version of the project to the prodction
+
+SYNC_DIR="$1"
+
+if [ -n $SYNC_DIR ]; then
+    echo "Sync directory not found passed as argument"
+    exit 1
+fi
+
+# Checking the directory existence in server 
+if [ ! -d $SYNC_DIR ]; then
+    echo "Sync directory not found on server"
+    exit 1
+fi
+
+# Finally copying the env file to the production dir
+
+cp $SYNC_DIR/.env $SYNC_DIR/production/.env
+
+# Changing to the deployment directory
+cd $SYNC_DIR/production
+
+#  Checking for previously running pm2 instances
+if [[ -n $(pm2 ls | grep "applicat") ]]; then
+    echo "Applicat is not running starting applicat"
+    pm2 start "bun run start" --name "applicat" -i max
+    exit 1
+else
+    echo "Applicat is Already running restarting"
+    pm2 restart "applicat"
+fi
