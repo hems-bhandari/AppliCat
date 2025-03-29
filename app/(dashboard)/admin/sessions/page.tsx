5e165f10e1dd566c764c804fbf7c206f77f2a2ac
sarojregmi200 <sarojregmi.official@gmail.com>
2025-03-29 22:37:47 +0545

    fix: build error solved for now

--- app/(dashboard)/admin/sessions/page.tsx
+++ app/(dashboard)/admin/sessions/page.tsx
@@ -1,81 +1,8 @@
 "use client"
 
-import { DataTable } from "@/components/ui/data-table";
-import { Checkbox } from "@radix-ui/react-checkbox";
-import { ColumnDef } from "@tanstack/react-table";
-
 const AdminSessionPage = () => {
-    type TableHeader = {
-        checkbox: boolean,
-        consultantName: string,
-        sessionTitle: string,
-        applicantName: string,
-        sessionRate: string,
-        sessionTime: string,
-        sessionDate: string,
-        status: "pending" | "confirmed" | "progress",
-    }
-
-    const column: ColumnDef<TableHeader>[] = [{
-        accessorKey: "checkbox",
-        cell: () => {
-            return <Checkbox />
-        }
-    },
-    {
-        accessorKey: "sessionTitle",
-        header: "Session"
-    },
-    {
-        accessorKey: "consultantName",
-        header: "Session By",
-    },
-    {
-        accessorKey: "applicantName",
-        header: "Applicant",
-    },
-    {
-        accessorKey: "sessionRate",
-        header: "Session Charge",
-    },
-    {
-        accessorKey: "sessionTime",
-        header: "Time"
-    },
-    {
-        accessorKey: "sessionDate",
-        header: "Date"
-    },
-    {
-        accessorKey: "status",
-        header: "Status"
-    },
-    ]
-
-
     return <div>
-        <DataTable
-            searchKey="sessionTitle"
-            className={
-                {
-                    scrollArea: "mt-5",
-                    search: "w-full md:max-w-none"
-                }
-            }
-            columns={column}
-            data={[
-                {
-                    checkbox: true,
-                    consultantName: "test",
-                    applicantName: "Testing ",
-                    sessionDate: "bholi",
-                    sessionTime: "10pm",
-                    sessionRate: "10 khoka",
-                    sessionTitle: "why should I say?",
-                    status: "pending"
-                }
-            ]} />
-
+        Session Page here
     </div>;
 }
 
