44c362ef7c57a4f69b87c0126500dfe4d843fe11
sarojregmi200 <sarojregmi.official@gmail.com>
2025-03-30 20:37:34 +0545

    deploy: Squashed all the commits into one.
    Squashed all the commits into one because I was kinda debugging what
    went wrong. Here are the messages in chronological order:
    
    deploy: testing pm2 availablility by specifying it's exact path.
    deploy: Sourcing the bashrc first to load path correctly
    deployment: checking path
    deploy: Manually adding bun bin to path
    deploy: debugging path issue
    deploy: corrected the silly bash variable mistake.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,9 @@
 # Deploys the current version of the project to the prodction
 
 SYNC_DIR="$1"
+SERVER_USER="$2"
+
+PATH=$PATH:/home/$SERVER_USER/.bun/bin/
 
 if [ -z "$SYNC_DIR" ]; then
     echo "Sync directory not found passed as argument"
@@ -24,11 +27,10 @@ cd $SYNC_DIR/production
 
 #  Checking for previously running pm2 instances
 if [[ -z $(pm2 ls | grep "applicat") ]]; then
-    echo "Applicat is not running starting applicat"
+    echo "Applicat is not running starting it"
     pm2 start "bun run start" --name "applicat" -i 1
     exit 1
 else
     echo "Applicat is Already running restarting"
     pm2 restart "applicat"
 fi
-
